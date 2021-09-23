3afb55b5130b415c5ea0e20271c0f12326c25376
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-09-23 16:30:38 +0200

    Make mod class final

--- src/main/java/io/github/noeppi_noeppi/mods/bingolobby/BingoLobby.java
+++ src/main/java/io/github/noeppi_noeppi/mods/bingolobby/BingoLobby.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import javax.annotation.Nonnull;
 
 @Mod("bingolobby")
-public class BingoLobby extends ModXRegistration {
+public final class BingoLobby extends ModXRegistration {
     
     private static BingoLobby instance;
     private static LobbyNetwork network;
